e1f82c6934866cd0b68dbe92080978b2e58a52e0
martinaliporata <martinaliporata@gmail.com>
2024-05-23 20:42:52 +0200

    creato array di oggetti

---
+++ Snack 1/js/script.js
@@ -0,0 +1,70 @@
+// SNACK 1
+// Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+// Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+// [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+// Ma  la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto letterale javascript che ha come proprietà:
+// nome del tavolo (tableName),
+// nome dell'ospite (guestName),
+// posto occupato (place),
+// Generiamo e stampiamo in console la lista per i segnaposto.
+
+// Creiamo un array di oggetti dal nome "Tavolo Vip" contenente i nomi dei partecipanti [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'] ognuno con tableName, guestName e place
+// Stampiamo in console l'array
+
+const tavoloVip = [
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Brad Pitt',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Johnny Depp',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName:'Lady Gaga',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Cristiano Ronaldo',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: ' Georgina Rodriguez',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName:'Chiara Ferragni',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'George Clooney',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Amal Clooney',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName:'Fedez',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Amadeus',
+        place:
+    },
+    {
+        tableName: 'tavoloVip',
+        guestName: 'Fiorello',
+        place:
+    },
+]
---
+++ Snack 2/js/script.js
@@ -0,0 +1,18 @@
+// SNACK 2
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+// 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo, ad esempio (Marco della Rovere => MARCO DELLA ROVERE);
+// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+// Questo è l'elenco degli studenti:
+// Id  Name                Grades
+// 213 Giuseppina della Rovere 78
+// 110 Paola Cortellessa       96
+// 250 Andrea Mantegna         48
+// 145 Gaia Borromini          74
+// 196 Luigi Grimaldello       68
+// 102 Piero della Francesca   50
+// 120 Francesca da Polenta    84
+
+// Creiamo un array di oggetti, ognuno dei quali avrà un id, un name (scritto in minuscolo) e un grade
+// Con filter creiamo una lista degli studenti con media voti >70
+// Con un altro filter creiamo una lista degli studenti con media voti >70 e un id >120
