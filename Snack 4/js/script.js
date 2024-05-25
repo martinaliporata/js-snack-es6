63aec5a4d54d198fb8f8a3225d90856dae02bec9
martinaliporata <martinaliporata@gmail.com>
2024-05-25 10:43:36 +0200

    inserito prompt per gli utenti

--- Snack 4/js/script.js
+++ Snack 4/js/script.js
@@ -4,6 +4,58 @@
 // Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
-// Creo un array di quadra da calcio, ognuna delle quali avrà le seguenti proprietà (nome - da compilare, punti fatti e falli subiti - settati a 0).
+// Creo un array di squadre da calcio, ognuna delle quali avrà le seguenti proprietà (nome - da compilare, punti fatti e falli subiti - settati a 0).
 // Al posto degli 0, generare un numero random con math.random
 // Creiamo un nuovo array creando un destructuring contenente solo nome e falli subiti dalla squadra e stampiamo in console.
+
+const arraySquadraCalcio = [
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: '',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+]
+
+
+let userInput = prompt("Please enter your name");
+arraySquadraCalcio.push = userInput;
