91a40617280a3e8d8c32853f70e40c1238e69dc7
martinaliporata <martinaliporata@gmail.com>
2024-05-25 10:34:54 +0200

    creato array di oggetti

---
+++ Snack 3/js/script.js
@@ -0,0 +1,35 @@
+// SNACK 3
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+// Creo un array di oggetti che hanno come proprietà nome e peso 
+// Utilizzando il destructuring (quindi dichiarando una variabile con all'interno tante chiavi
+// quante le proprietà che vogliamo estrapolare) e il backtick, stampiamo in console
+
+const arrayBici = [
+    {
+        nome: 'bici1',
+        peso: 6
+    },
+    {
+        nome: 'bici2',
+        peso: 7
+    },
+    {
+        nome: 'bici3',
+        peso: 8
+    },
+    {
+        nome: 'bici4',
+        peso: 9
+    },
+    {
+        nome: 'bici5',
+        peso: 10
+    },
+    {
+        nome: 'bici6',
+        peso: 11
+    }
+]
